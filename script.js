5481f4b57045cca7b2b48d252a4de6fb0ff9d5b7
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-03-17 12:49:24 -0400

    expand wordlists

--- script.js
+++ script.js
@@ -5,20 +5,75 @@ generateBtn.addEventListener('click', () => {
 	// get the number of words to generate from the input field
 	const numWords = parseInt(document.getElementById('num-words').value);
 
-	var wordlistStr = `doctor
+	var wordlistStr = `
+doctor
 sir
 professor
 coach
 verner von
+His majesty
+Queen
+King
+Mrs
+Prince
+Princess
+President
+Vice principal
+Senator
+Lord
+Lady
+Secretary
+Congressman
+Representative
+MP
+Judge
+Prime minister
+Minister
+Principal
+Vice president
+Speaker
+Chef
+Warden
 
 stephen
 evelyn
 martin
+randolph
+richard
+suzanne
+rachel
+Jared
+Barnaby
+Elizabeth
+Harold
+Sarah
+Kevin
+Aaron
+Tina
 
 meatballs
 lasagne
 sauerkraut
 dishwasher
+refridgerator
+Spinach
+Crackers
+Biscuit
+Meatloaf
+Sandwich
+Futon
+Hotdog
+Tofu
+Custard
+Jell-O
+Horseradish
+Beetroot
+Potato
+Cookie
+Chicken
+Rooster
+Turkey
+Cheesecake
 `;
 	// split the wordlist string into an array of wordlists
 	let wordlists = wordlistStr.trim().replace(/\n{2}/g, '~').split('~');
