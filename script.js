518d54082ca9c0f8ca2cfe9e4691888404c89a04
Adrian Edwards <adrian@adriancedwards.com>
2024-03-25 21:26:58 -0400

    convert to a class format

--- script.js
+++ script.js
@@ -125,157 +125,56 @@ function addPunToContainer(pun, container) {
 	container.appendChild(details);
 }
 
-var dwaynePuns = [
-	{
-		setup: "What would you call Dwayne Johnson if he was a Trekkie/star trek fan?",
-		punchline: "Dwayne the Spock Johnson"
-	},
-	{
-		setup: "What would you call Dwayne Johnson if he slipped down a hill?",
-		punchline: "Rockslide"
-	},
-	{
-		setup: "What do you call Dwayne Johnson with a black belt?",
-		punchline: "Dwayne \"The block\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he doesnt want to see someone on social media anymore?",
-		punchline: "Dwayne \"The block\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson rolling down a hill?",
-		punchline: "Rock n’ Roll"
-	},
-	{
-		setup: "What do you call Dwayne Johnson in a rooster costume?",
-		punchline: "Dwayne \"the Cock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson the gun enthusiast?",
-		punchline: "Dwayne \"the glock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson if he was an avian predator?",
-		punchline: "Dwayne \"the hawk\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he visits someone’s house?",
-		punchline: "Dwayne the knock Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson with tortilla chips?",
-		punchline: "Dwayne \"the guac[amole]\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he goes for a gentle stroll?",
-		punchline: "Dwayne the walk Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he is on time?",
-		punchline: "Dwayne \"the clock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he is holding onto a boat?",
-		punchline: "Dwayne \"the Dock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he‘s staring at something in amazement?",
-		punchline: "Dwayne the gawk Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he gets high?",
-		punchline: "Stoned"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he is protecting his stuff?",
-		punchline: "Dwayne \"the lock\" Johnson"
-	},
-	{
-		setup: "What do you call an athletic Dwayne Johnson?",
-		punchline: "Dwayne \"the jock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson if he was a supermarket?",
-		punchline: "Dwayne \"the Stock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson the investor?",
-		punchline: "Dwayne \"the Stock\" Johnson",
-		source: "https://www.reddit.com/r/MemeEconomy/comments/6guj6t/dwayne_the_rock_johnson_memes_on_the_rise/"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he is keeping food in a bag/storing leftovers?",
-		punchline: "Dwayne \"the ziplock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he is trying to debug something/the system admin/tree?",
-		punchline: "Dwayne \"the log\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson the house elf?",
-		punchline: "Dwayne \"the sock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson the handgun enthusiast?",
-		punchline: "Dwayne \"the glock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he comes to visit?",
-		punchline: "Dwayne \"the knock knock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he works as a student employee?",
-		punchline: "Dwayne \"the on the clock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he's a social media star?",
-		punchline: "Dwayne \"the Tik Tok\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he plays Minecraft?",
-		punchline: "Dwayne \"the Bedrock\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he plays Minecraft?",
-		punchline: "Dwayne \"the Block\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he wears closed-toed sandals?",
-		punchline: "Dwayne \"the Croc\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson the marine animal?",
-		punchline: "The Rock-topus"
-	},
-	{
-		setup: "What do you call Dwayne Johnson after spending many extra years in school?",
-		punchline: "Dwayne \"the Postdoc\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson after quitting wrestling to pursue more classical interests?",
-		punchline: "Dwayne \"the Bach\" Johnson"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he is helping make a large stir fry?",
-		punchline: "Dwayne \"the Wok\" Johnson",
-		source: "https://www.reddit.com/r/me_irl/comments/6gn3ph/me_irl/"
-	},
-	{
-		setup: "What do you call Dwayne Johnson humorously pretenting to be something he is not?",
-		punchline: "Dwayne \"the Mock\" Johnson",
-		source: "https://knowyourmeme.com/photos/1362206-dwayne-the-rock-johnson-rhymes"
+class DwaynePun {
+	constructor(setup, punchline, source = "", is_alternative = false) {
+		this.setup = setup;
+		this.punchline = punchline;
+		this.source = source;
+		this.is_alternative = is_alternative;
+	}
 
-	},
-	{
-		setup: "What do you call Dwayne Johnson keeping his kid safe from unnecessary dirt?",
-		punchline: "Dwayne \"the Smock\" Johnson",
-		source: "https://knowyourmeme.com/photos/1275520-dwayne-the-rock-johnson-rhymes"
-	},
-	{
-		setup: "What do you call Dwayne Johnson when he takes 6-8 hours to cook his food?",
-		punchline: "Dwayne \"the Crock [Pot]\" Johnson",
-		source: "https://knowyourmeme.com/photos/1265292-dwayne-the-rock-johnson-rhymes"
-	},
+}
+
+
+var dwaynePuns = [
+
+	new DwaynePun("What would you call Dwayne Johnson if he was a Trekkie/star trek fan?", "Dwayne \"the Spock\" Johnson"),
+	new DwaynePun("What would you call Dwayne Johnson if he slipped down a hill?", "Rockslide", "", true),
+	new DwaynePun("What do you call Dwayne Johnson with a black belt?", "Dwayne \"The block\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he doesnt want to see someone on social media anymore?", "Dwayne \"The block\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson rolling down a hill?", "Rock n’ Roll", "", true),
+	new DwaynePun("What do you call Dwayne Johnson in a rooster costume?", "Dwayne \"the Cock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson the gun enthusiast?", "Dwayne \"the glock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson if he was an avian predator?", "Dwayne \"the hawk\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he visits someone’s house?", "Dwayne the knock Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson with tortilla chips?", "Dwayne \"the guac[amole]\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he goes for a gentle stroll?", "Dwayne the walk Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he is on time?", "Dwayne \"the clock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he is holding onto a boat?", "Dwayne \"the Dock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he‘s staring at something in amazement?", "Dwayne the gawk Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he gets high?", "Stoned", "", true),
+	new DwaynePun("What do you call Dwayne Johnson when he is protecting his stuff?", "Dwayne \"the lock\" Johnson"),
+	new DwaynePun("What do you call an athletic Dwayne Johnson?", "Dwayne \"the jock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson if he was a supermarket?", "Dwayne \"the Stock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson the investor?", "Dwayne \"the Stock\" Johnson", "https://www.reddit.com/r/MemeEconomy/comments/6guj6t/dwayne_the_rock_johnson_memes_on_the_rise/"),
+	new DwaynePun("What do you call Dwayne Johnson when he is keeping food in a bag/storing leftovers?", "Dwayne \"the ziplock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he is trying to debug something/the system admin/tree?", "Dwayne \"the log\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson the house elf?", "Dwayne \"the sock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson the handgun enthusiast?", "Dwayne \"the glock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he comes to visit?", "Dwayne \"the knock knock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he works as a student employee?", "Dwayne \"the on the clock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he's a social media star?", "Dwayne \"the Tik Tok\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he plays Minecraft?", "Dwayne \"the Bedrock\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he plays Minecraft?", "Dwayne \"the Block\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he wears closed-toed sandals?", "Dwayne \"the Croc\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson the marine animal?", "The Rock-topus", "", true),
+	new DwaynePun("What do you call Dwayne Johnson after spending many extra years in school?", "Dwayne \"the Postdoc\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson after quitting wrestling to pursue more classical interests?", "Dwayne \"the Bach\" Johnson"),
+	new DwaynePun("What do you call Dwayne Johnson when he is helping make a large stir fry?", "Dwayne \"the Wok\" Johnson", "https://www.reddit.com/r/me_irl/comments/6gn3ph/me_irl/"),
+	new DwaynePun("What do you call Dwayne Johnson humorously pretenting to be something he is not?", "Dwayne \"the Mock\" Johnson", "https://knowyourmeme.com/photos/1362206-dwayne-the-rock-johnson-rhymes"
+),
+	new DwaynePun("What do you call Dwayne Johnson keeping his kid safe from unnecessary dirt?", "Dwayne \"the Smock\" Johnson", "https://knowyourmeme.com/photos/1275520-dwayne-the-rock-johnson-rhymes"),
+	new DwaynePun("What do you call Dwayne Johnson when he takes 6-8 hours to cook his food?", "Dwayne \"the Crock [Pot]\" Johnson", "https://knowyourmeme.com/photos/1265292-dwayne-the-rock-johnson-rhymes"),
 
 ];
 
