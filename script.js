f2ed40e05de7aab17c9a6dbbf049212232f4de5d
Adrian Edwards <adrian@adriancedwards.com>
2024-03-25 21:03:12 -0400

    format data

--- script.js
+++ script.js
@@ -128,79 +128,104 @@ function addPunToContainer(pun, container) {
 var dwaynePuns = [
 	{
 		setup: "What would you call Dwayne Johnson if he was a Trekkie/star trek fan?",
-		punchline: "Dwayne the Spock Johnson"},
+		punchline: "Dwayne the Spock Johnson"
+	},
 	{
 		setup: "What would you call Dwayne Johnson if he slipped down a hill?",
-		punchline: "Rockslide"},
+		punchline: "Rockslide"
+	},
 	{
 		setup: "What do you call Dwayne Johnson with a black belt?",
-		punchline: "Dwayne \"The block\" Johnson"},
+		punchline: "Dwayne \"The block\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he doesnt want to see someone on social media anymore?",
-		punchline: "Dwayne \"The block\" Johnson"},
+		punchline: "Dwayne \"The block\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson rolling down a hill?",
-		punchline: "Rock n’ Roll"},
+		punchline: "Rock n’ Roll"
+	},
 	{
 		setup: "What do you call Dwayne Johnson in a rooster costume?",
-		punchline: "Dwayne \"the Cock\" Johnson"},
+		punchline: "Dwayne \"the Cock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson the gun enthusiast?",
-		punchline: "Dwayne \"the glock\" Johnson"},
+		punchline: "Dwayne \"the glock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson if he was an avian predator?",
-		punchline: "Dwayne \"the hawk\" Johnson"},
+		punchline: "Dwayne \"the hawk\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he visits someone’s house?",
-		punchline: "Dwayne the knock Johnson"},
+		punchline: "Dwayne the knock Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson with tortilla chips?",
-		punchline: "Dwayne \"the guac[amole]\" Johnson"},
+		punchline: "Dwayne \"the guac[amole]\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he goes for a gentle stroll?",
-		punchline: "Dwayne the walk Johnson"},
+		punchline: "Dwayne the walk Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he is on time?",
-		punchline: "Dwayne \"the clock\" Johnson"},
+		punchline: "Dwayne \"the clock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he is holding onto a boat?",
-		punchline: "Dwayne the dock Johnson"},
+		punchline: "Dwayne the dock Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he‘s staring at something in amazement?",
-		punchline: "Dwayne the gawk Johnson"},
+		punchline: "Dwayne the gawk Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he gets high?",
-		punchline: "Stoned"},
+		punchline: "Stoned"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he is protecting his stuff?",
-		punchline: "Dwayne \"the lock\" Johnson"},
+		punchline: "Dwayne \"the lock\" Johnson"
+	},
 	{
 		setup: "What do you call an athletic Dwayne Johnson?",
-		punchline: "Dwayne \"the jock\" Johnson"},
+		punchline: "Dwayne \"the jock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson if he was a supermarket?",
-		punchline: "Dwayne the stock Johnson"},
+		punchline: "Dwayne the stock Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he is keeping food in a bag/storing leftovers?",
-		punchline: "Dwayne \"the ziplock\" Johnson"},
+		punchline: "Dwayne \"the ziplock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he is trying to debug something/the system admin/tree?",
-		punchline: "Dwayne \"the log\" Johnson"},
+		punchline: "Dwayne \"the log\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson the house elf?",
-		punchline: "Dwayne \"the sock\" Johnson"},
+		punchline: "Dwayne \"the sock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson the handgun enthusiast?",
-		punchline: "Dwayne \"the glock\" Johnson"},
+		punchline: "Dwayne \"the glock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he comes to visit?",
-		punchline: "Dwayne \"the knock knock\" Johnson"},
+		punchline: "Dwayne \"the knock knock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he works as a student employee?",
-		punchline: "Dwayne \"the on the clock\" Johnson"},
+		punchline: "Dwayne \"the on the clock\" Johnson"
+	},
 	{
 		setup: "What do you call Dwayne Johnson when he's a social media star?",
-		punchline: "Dwayne \"the Tik Tok\" Johnson"},
+		punchline: "Dwayne \"the Tik Tok\" Johnson"
+	},
 ];
 
 
